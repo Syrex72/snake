e942818e3648374c5513d778a57d39db76127b88
Syrex72 <136921293+Syrex72@users.noreply.github.com>
2024-09-05 20:38:31 +0000

    atualizaçõs de mapa e movimentação, fazer alteração no tamanho do mapa e ajuste de movimento, após tratardas frutas e pontuação

--- main.lua
+++ main.lua
@@ -1,44 +1,112 @@
 local love = require 'love'
 local button = require 'Button'
 
-local border = {
-	x = 5,
-	y = 5,
-	width = love.graphics.getWidth() - 10,
-	height = love.graphics.getHeight() - 10,
-	rx = 10,
-	ry = 10
+-- alterar tamanho para fazer proporção 12x20 quadrados
+local game = {
+	border = {
+		x = 10,
+		y = 10,
+		width = 410,
+		height = 410,
+	},
+	points = 0
 }
 
 local snake = {
-	size = 2,
-	points = 0,
+	head = {
+		direction = "right",
+		x = 200,
+		y = 200
+	},
+	tail = {},
+	speed = 10,
+	size = 10,
+	points = 1,
+
 }
 
-local buttons = {
-	start_game = {},
-	paused_game = {},
-	ended_game = {}
+local time = {
+	last = 0,
+	current = 0,
 }
+
+print(game.border.width - game.border.x .. ", " .. game.border.height - game.border.y)
+print(snake.head.x .. ", " .. snake.head.y )
+
 
 function love.load()
 	love.window.setTitle("Snake")
-
-	buttons.start_game = button()
+	
+	-- pega o tempo inicial
+	time.last = love.timer.getTime()
 end
 
 function love.update(dt)
+	-- atualiza o tempo, por frame
+	time.current = love.timer.getTime() 
+
+	-- garante que as ações so sejam executadas a cada 0.25 segundos
+	-- obtem esse tempo por meio da subtração do ultimo frame de movimento salvo e o atual
+	if time.current - time.last >= 0.25 then
+		-- print(time.current - time.last)w
+		if snake.head.direction == "up" then
+			-- verificação simples se ele 'saiu' dos limites da borda, repete para todas as direções
+			if snake.head.y - snake.speed < game.border.y then
+				snake.head.y = game.border.height
+			else 
+				snake.head.y = snake.head.y - snake.speed
+			end
+
+		elseif snake.head.direction == "right" then
+			if snake.head.x + snake.speed > game.border.width then
+				snake.head.x = game.border.x
+			else
+				snake.head.x = snake.head.x + snake.speed 
+			end
+
+		elseif snake.head.direction == "down" then
+			if snake.head.y + snake.speed > game.border.height then
+				snake.head.y = game.border.y
+			else
+				snake.head.y = snake.head.y + snake.speed
+			end
+
+		elseif snake.head.direction == "left" then
+			if snake.head.x - snake.speed < game.border.x then
+				snake.head.x = game.border.width
+			else
+				snake.head.x = snake.head.x - snake.speed
+			end
+		end
+
+		-- atualiza o ultimo frame de movimento
+		time.last = time.current
+	end
 	
 end
 
 function love.keypressed(key)
-	
+	if key == "w" and snake.head.direction ~= "down" then
+		snake.head.direction = "up"
+	elseif key == "d" and snake.head.direction ~= "left" then
+		snake.head.direction = "right"
+	elseif key == "s" and snake.head.direction ~= "up" then
+		snake.head.direction = "down"
+	elseif key == "a" and snake.head.direction ~= "right" then
+		snake.head.direction = "left"
+	end
 end
 
 function love.draw()
-	love.graphics.setColor(1, 0, 0)
-	love.graphics.rectangle("line", border.x, border.y, border.width, border.height, border.rx, border.ry)
-
+	-- mapa 
 	love.graphics.setColor(1, 1, 1)
-	love.graphics.print("teste", 100, 100)
+	love.graphics.rectangle("fill", snake.head.x, snake.head.y, snake.size, snake.size)
+	
+	-- jogador
+	love.graphics.setColor(1, 0, 0)
+	love.graphics.rectangle("line", game.border.x, game.border.y, game.border.width, game.border.height)
+
+	
+
+
 end
